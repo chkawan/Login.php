acf467528b7bac72fb5455acdc250ed47977c163
chkawan <chris7kw@hotmail.com>
2023-02-18 12:20:05 -0300

    modified:   index.php 	cadastro.php 	css/ 	js/ 	links_footer.php 	links_head.php 	recuperar.php

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
 <!DOCTYPE html>
 <head>
-    <title></title>
+    <title>Niveis de Acesso</title>
 </head>
 
 <body>
-    
+    <div class="container">
+
+        <h1>Vamos comerçar?</h1>
+
+        <div class="grup">
+
+            <label for="login" class="input-grup">
+                Login:
+                    <input type="tex" name="login" id="login">
+            </label>
+
+            <label for="password" class="input-grup">
+                Password:
+                    <input type="password" name="password" id="password">
+            </label>
+
+            <input type="button" value="Conectar" id="conect" class="button">
+        
+        </div>
+
+    </div>
 </body>
